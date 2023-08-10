ba90a5c42aeebe55b0d2a6d1b945d0e2e092546a
isaias garcia <isaico@outlook.com>
2023-08-10 19:52:40 -0300

    build en pregreso de deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {QueryClientProvider,QueryClient} from "@tanstack/react-query"
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
